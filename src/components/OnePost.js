967e508c1aad41a16918e6c71d53980e9f68be60
RoshBJr <roshanjr3@gmail.com>
2024-01-20 12:30:34 -0500

    just finished blog post introductory project, used bit of tailwind

--- src/components/OnePost.js
+++ src/components/OnePost.js
@@ -1,10 +1,10 @@
-// src/components/OnePost.js
+// src/component/OnePost.js
 
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
 import BlockContent from "@sanity/block-content-to-react";
 import imageUrlBuilder from "@sanity/image-url";
-import sanityClient from '../client.js';
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -16,21 +16,21 @@ export default function OnePost() {
   const { slug } = useParams();
 
   useEffect(() => {
-    sanityClient.fetch(
-        `*[slug.current == $slug]{
-          title,
-          slug,
-          mainImage{
-            asset->{
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{
+           title,
+           slug,
+           mainImage{
+           asset->{
               _id,
               url
-             }
-           },
-         body,
-        "name": author->name,
-        "authorImage": author->image
-       }`,
-        { slug }
+            }
+          },
+          body,
+          "name": author->name,
+          "authorImage": author->image
+       }`
       )
       .then((data) => setPostData(data[0]))
       .catch(console.error);
@@ -39,20 +39,41 @@ export default function OnePost() {
   if (!postData) return <div>Loading...</div>;
 
   return (
-    <div>
-      <div>
-        <h2>{postData.title}</h2>
-        <div>
-          <h4>{postData.name}</h4>
+    <div className="bg-gray-200 min-h-screen p-12">
+      <div className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <div className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            {/* Title Section */}
+            <div className="bg-white bg-opacity-75 rounded p-12">
+              <h2 className="cursive text-3xl lg:text-6xl mb-4">
+                {postData.title}
+              </h2>
+              <div className="flex justify-center text-gray-800">
+                <img
+                  src={urlFor(postData.authorImage).url()}
+                  className="w-10 h-10 rounded-full"
+                  alt="Author is Kap"
+                />
+                <h4 className="cursive flex items-center pl-2 text-2xl">
+                  {postData.name}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-full object-cover object-top rounded-t"
+            src={urlFor(postData.mainImage).url()}
+            alt=""
+            style={{ height: "400px" }}
+          />
         </div>
-      </div>
-      <img src={urlFor(postData.mainImage).width(200).url()} alt="" />
-      <div>
-        <BlockContent
-          blocks={postData.body}
-          projectId={sanityClient.projectId}
-          dataset={sanityClient.dataset}
-        />
+        <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+          <BlockContent
+            blocks={postData.body}
+            projectId={sanityClient.projectId}
+            dataset={sanityClient.dataset}
+          />
+        </div>
       </div>
     </div>
   );
